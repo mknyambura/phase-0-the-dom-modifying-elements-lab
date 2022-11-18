424b9c79e8ef10d82397c729305d505670291e19
mknyambura <kariuki.mercie@gmail.com>
2022-11-18 11:32:31 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+const getMain = document.getElementById('main');
+getMain.remove();
+
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.textContent = "Mercy Nyambura is the champion";
+
+const body = document.querySelector('body');
+body.appendChild(newHeader);
